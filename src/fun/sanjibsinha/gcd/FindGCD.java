51559b71118812ec136e2ba809326e6f6bec9587
root <sanjib12sinha@gmail.com>
2020-01-12 11:06:44 +0530

    GCD commit

--- src/fun/sanjibsinha/gcd/FindGCD.java
+++ src/fun/sanjibsinha/gcd/FindGCD.java
@@ -1,4 +1,46 @@
 package fun.sanjibsinha.gcd;
 
 public class FindGCD {
+
+    static int numOne = 0;
+    static int numTwo = 0;
+    static int remain = 0;
+
+    static int divisionBased(int num1, int num2){
+        int temp;
+        while (num2 != 0){
+            temp = num2;
+            num2 = num1 % num2;
+            num1 = temp;
+        }
+        return num1;
+    }
+
+    //this is Euclid's original version
+    static int subtractionBased(int num1, int num2){
+        int temp;
+        while (num1 != num2){
+            if(num1 > num2)
+                num1 = num1 - num2;
+            else
+                num2 = num2 - num1;
+        }
+        return num1;
+    }
+
+    static int recursiveBased(int num1, int num2){
+        if(num2 == 0){
+            return num1;
+        } else {
+            int temp;
+            temp = num1 % num2;
+            return recursiveBased(num2, temp);
+        }
+    }
+
+    public static void main(String[] args) throws ArithmeticException {
+
+        System.out.println(recursiveBased(1071, 462));
+
+    }
 }
